7ef0e9e8925759e70ce3ebc8d8b7e64e62bafda1
admin <admin@C11>
2024-03-29 21:45:23 +0900

    0329

--- Ex_0329/src/ex5_phone/Phone.java
+++ Ex_0329/src/ex5_phone/Phone.java
@@ -1,5 +1,17 @@
 package ex5_phone;
 
 public class Phone {
-
+	String brand;
+	int series;
+	String color = "검정색";
+	
+	public Phone(String b, int s, String c) {
+		brand = b;
+		series = s;
+		color = c;
+	}
+	
+	public void phoneInfo() {
+		System.out.println(color + " " + brand + " " + series);
+	}
 }
