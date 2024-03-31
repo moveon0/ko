36a544002904d742b861e20433ebeb15c23fdcd8
young jin Yun <yunyj1224@gmail.com>
2024-04-01 02:05:05 +0900

    0401

--- work/src/work/PrintGraph.java
+++ work/src/work/PrintGraph.java
@@ -1,13 +1,15 @@
 package work;
-//2차원 리스트 만들고 [~의개수][해당 숫자]
-//반복문으로 배열을 받고 2차원 리스트에 저장
-//count로 숫자 개수 세기dfs
-//배열의 크기만큼 .length 로 '#'출력
+// 숫자 개수 세는 c*들을 c 리스트로 만든다.
+// 랜덤 정수가 들어올 때마다 해당 c를 +1한다.
+//0~9니깐 10으로 반복문 돌린다.
+//i가 0~9가 되면서 c[0]~c[9]의 정수 개수만큼 '#'을 출력한다
+//해당 정수의 개수는 c[*]의 길이로 표현한다.
+//끝!
 public class PrintGraph {
 	public void PrintGraph(int [] arr) {
 		int c0=0,c1=0,c2=0,c3=0,c4=0,c5=0,c6=0,c7=0,c8=0,c9=0;
 		int [] c= {c0,c1,c2,c3,c4,c5,c6,c7,c8,c9};
-		int [][] ar= new int [10][30];
+		
 		for (int i = 0 ; i < arr.length; i++) {
 			switch(arr[i]) {
 				case 0:
